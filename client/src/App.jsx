5ae6a23bb04896e689a4a9f25c267718e10f5b0f
Apogee <saiganeshs902@gmail.com>
2024-06-19 22:01:49 +0530

    feat2-added-timer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import VideoPlayer from './VideoComponent'
 function App() {
-
+  const [currentTime, setCurrentTime] = useState(0);
+  useEffect(() => {
+    const getTime = async () => {
+      const response = await fetch('http://localhost:3000/time')
+      const data = await response.json()
+      setCurrentTime(data.time)
+      console.log(data.time)
+    }
+    getTime()
+  });
   return (
     <>
-      <VideoPlayer  />
+      <VideoPlayer currentTime={currentTime}  />
     </>
   )
 }
--- client/src/VideoComponent.jsx
+++ client/src/VideoComponent.jsx
@@ -1,37 +1,36 @@
-import { useEffect, useRef, useState } from 'react';
-import io from 'socket.io-client';
+/* eslint-disable react/prop-types */
+import  { useEffect, useRef } from 'react';
 
-const VideoPlayer = () => {
+const VideoPlayer = ({currentTime}) => {
     const videoRef = useRef(null);
-    const socketRef = useRef(null);
-    const [timestamp, setTimestamp] = useState(0);
-
+    const videoSrc = 'http://localhost:3000/movie.mp4'
+    
     useEffect(() => {
-        // Connect to the socket.io server
-        socketRef.current = io('http://localhost:3000');
+        if (videoRef.current) {
+            videoRef.current.src = videoSrc;
+            videoRef.current.currentTime = currentTime;
+            console.log('videoRef.current.currentTime', videoRef.current.currentTime);
+            videoRef.current.play();
+        }
+    }, [currentTime]);
 
-        socketRef.current.on('currentTimestamp', (serverTimestamp) => {
-            setTimestamp(serverTimestamp);
-        });
-
-        socketRef.current.on('video', (chunk) => {
-            if (videoRef.current) {
-                const blob = new Blob([chunk], { type: 'video/mp4' });
-                const url = URL.createObjectURL(blob);
-                videoRef.current.src = url;
-                videoRef.current.currentTime = timestamp;
-                videoRef.current.play();
-            }
-        });
-
-        return () => {
-            socketRef.current.disconnect();
-        };
-    }, [timestamp]);
+    const stopVideo = () =>{
+        // if video is playing, stop it
+        if (!videoRef.current.paused) {
+            videoRef.current.pause();
+        }
+        else{
+            // resume video
+            videoRef.current.play();
+        }
+    }
 
     return (
         <div>
+            <button id="setTime" onClick={() => videoRef.current.currentTime = 10}>Set Time</button>
             <video ref={videoRef} controls autoPlay />
+            {/* <button id="playVideo" onClick={playVideo}>Play Video</button> */}
+            <button id="stopVideo" onClick={stopVideo}>Stop Video</button>
         </div>
     );
 };
